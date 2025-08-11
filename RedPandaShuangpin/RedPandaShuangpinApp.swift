4e5a2b44dfc98c07a3aa0b15d001c739b4119a20
Jiyu Mochi He <jiyuh95@gmail.com>
2025-08-11 16:00:18 -0700

    Initial Commit

---
+++ RedPandaShuangpin/RedPandaShuangpinApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedPandaShuangpinApp.swift
+//  RedPandaShuangpin
+//
+//  Created by Jiyu He on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RedPandaShuangpinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RedPandaShuangpinTests/RedPandaShuangpinTests.swift
@@ -0,0 +1,17 @@
+//
+//  RedPandaShuangpinTests.swift
+//  RedPandaShuangpinTests
+//
+//  Created by Jiyu He on 8/11/25.
+//
+
+import Testing
+@testable import RedPandaShuangpin
+
+struct RedPandaShuangpinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
